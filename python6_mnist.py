6bb76fd6d393b637ce13593394b8c2026a7fc6ac
edwardcullen <lx_einstein@sina.com>
2018-09-30 15:36:27 +0800

    update about mnist with tensorboard

--- python6_mnist.py
+++ python6_mnist.py
@@ -5,15 +5,20 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # number 1 to 10 data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-def add_layer(inputs, in_size, out_size, activation_function=None,):
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None,):
     # add one more layer and return the output of this layer
-    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1,)
-    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    layer_name = 'layer%s' % n_layer
+    with tf.name_scope('Weights'):
+        Weights = tf.Variable(tf.random_normal([in_size, out_size]), name = 'W')
+    with tf.name_scope('biases'):
+        biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name = 'b')
+    with tf.name_scope('Wx_plus_b'):
+        Wx_plus_b = tf.matmul(inputs, Weights) + biases
     if activation_function is None:
         outputs = Wx_plus_b
     else:
-        outputs = activation_function(Wx_plus_b,)
+        outputs = activation_function(Wx_plus_b)
+    tf.summary.histogram(layer_name+'/outputs', outputs)
     return outputs
 
 def compute_accuracy(v_xs, v_ys):
@@ -26,31 +31,43 @@ def compute_accuracy(v_xs, v_ys):
     return result
 
 # define placeholder for inputs to network
-xs = tf.placeholder(tf.float32, [None, 784]) # 28x28
-ys = tf.placeholder(tf.float32, [None, 10])
+with tf.name_scope('inputs'):
+    xs = tf.placeholder(tf.float32, [None, 784], name = 'x_input') # 28x28
+    ys = tf.placeholder(tf.float32, [None, 10], name = 'y_input')
 
 # add output layer
-prediction = add_layer(xs, 784, 10,  activation_function=tf.nn.softmax)
+prediction = add_layer(xs, 784, 10, n_layer=1,  activation_function=tf.nn.softmax)
 
 # the error between prediction and real data
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
-                                              reduction_indices=[1]))       # loss
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+with tf.name_scope('loss'):
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), reduction_indices=[1]))# loss
+    tf.summary.scalar('loss', cross_entropy)
+with tf.name_scope('train_step'):
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-sess = tf.Session()
-# important step
-sess.run(tf.global_variables_initializer())
+init_op = tf.global_variables_initializer()
 
-for i in range(101):
-    batch_xs, batch_ys = mnist.train.next_batch(1)
-    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
-    if i % 1 == 0:
-        print(compute_accuracy(
-            mnist.test.images, mnist.test.labels))
-        # print(i)
+with tf.Session() as sess:
 
-# 训练1000步
-# 0.098 每次10个数据
-# 0.8714 每次100个数据
-# 0.8814 每次1000个数据
-# 0.8779 每次10000个数据
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter("mnist_logs/train", sess.graph)
+    # test_writer = tf.summary.FileWriter("mnist_logs/test", sess.graph)
+    # important step
+    sess.run(init_op)
+
+    for i in range(10001):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
+        if i % 100 == 0:
+            # print(compute_accuracy(mnist.test.images, mnist.test.labels))
+            # record loss
+            train_result = sess.run(merged, feed_dict={xs: batch_xs, ys: batch_ys})
+            # test_result = sess.run(merged, feed_dict={xs: mnist.test.images, ys: mnist.test.labels})
+            train_writer.add_summary(train_result, i)
+            # test_writer.add_summary(test_result, i)
+
+    # 训练1000步
+    # 0.098 每次10个数据
+    # 0.8714 每次100个数据
+    # 0.8814 每次1000个数据
+    # 0.8779 每次10000个数据
